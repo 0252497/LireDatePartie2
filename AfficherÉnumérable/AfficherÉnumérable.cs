34cd64d8498a341cb2949c7a869e2da788335320
Veronique Giguere <0252497@cstjean.qc.ca>
2019-04-04 11:55:37 -0400

    FIN cours

---
+++ AfficherÉnumérable/AfficherÉnumérable.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using static Prog2.ConsolePlus;
+using static System.Console;
+using static Prog2.Mois;
+using System.Collections.Generic;
+using static Prog2.Date;
+
+namespace Prog2
+{
+    static class AfficherÉnumérable
+    {
+        static void Main(string[] args)
+        {
+            Title = "Afficher Énumérable";
+
+            Date date = new Date();
+
+            object              objet = date;
+            IEquatable<Date>    equatable = date;
+            IComparable<Date>   comparable = date;
+            IFormattable        formattable = date;
+
+            Afficher("Même objet",
+                ReferenceEquals(date, objet) &&
+                ReferenceEquals(objet, equatable) &&
+                ReferenceEquals(equatable, comparable) &&
+                ReferenceEquals(comparable, formattable));
+            
+            ICalendrier cal = new Calendrier(2000, Mai);
+
+            // IEnumerable polymorphe 1 :
+            int[] impairs = new[] { 1, 3, 5, 7, 9 };
+            List<int> pairs = new List<int> { 2, 4, 6, 8, 10 };
+            IEnumerable<int> entiers;
+            
+            // IEnumerable et foreach :
+            Write("\nEntiers: ");
+            entiers = pairs;
+
+            foreach (int entier in entiers)
+                Write(entier + " ");
+
+            entiers = impairs;
+
+            foreach (int entier in entiers)
+                Write(entier + " ");
+
+            WriteLine();
+
+            Write("\nEntiers: ");
+            WriteAll(pairs);
+            WriteAll(impairs);
+
+            WriteLine();
+
+            WriteLine($"\nEntiers: " + EnTexte(pairs) + " " + EnTexte(impairs));
+
+            // IEnumerable polymorphe 2
+            char[] voyelles = new[] { 'a', 'e', 'i', 'o', 'u' };
+            List<char> consonnes = new List<char> { 'b', 'c', 'd', 'f', 'g' };
+            string chiffres = "12345";
+            IEnumerable<char> caractères;
+            caractères = voyelles;
+            caractères = consonnes; caractères = chiffres;
+
+            Write("\nCaractères: ");
+            WriteAllChar(voyelles);
+            WriteAllChar(consonnes);
+            WriteAllChar(chiffres);
+            WriteLine();
+
+            Write("\nAny: ");
+            WriteAny<int>(new[] { 1, 2, 3 });    // T = int
+            WriteAny(new List<bool> { true, true, false });    // T = bool
+            WriteAny("C#");    // T = char
+            WriteAny(new[] { Hier, Demain });    // T = Date
+            WriteLine();
+        }
+
+        static string EnTexte(IEnumerable<int> p_entiers, string séparateur = " ")
+            => string.Join(séparateur, p_entiers);
+
+        // Méthode générique :
+        static void WriteAny<T>(IEnumerable<T> elems, string séparateur = " ")
+        {
+            foreach (T elem in elems)
+            {
+                Write(elem + séparateur);
+            }
+        }
+
+        // Paramètres IEnumerable 3
+        static void WriteAllChar(IEnumerable<char> p_chars, string séparateur = " ")
+        {
+            foreach (char c in p_chars)
+            {
+                Write(c + séparateur);
+            }
+        }
+
+        // Paramètre IEnumerable 1
+        static void WriteAll(IEnumerable<int> p_entiers, string séparateur = " ")
+        {
+            foreach (int entier in p_entiers)
+            {
+                Write(entier + séparateur);
+            }
+        }
+    }
+}
---
+++ UtilitaireLib/IEnumerableUtil.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog2
+{
+    static class IEnumerableUtil
+    {
+        /// <summary>
+        /// Convertit un énumérable en texte.
+        /// </summary>
+        /// <typeparam name="T">type générique quelconque</typeparam>
+        /// <param name="elems">les éléments énumérables</param>
+        /// <param name="séparateur">le sépareteur à insérer entre les éléments</param>
+        /// <param name="texteAvant">texte à ajouter devant chaque élément</param>
+        /// <param name="texteAprès">texte à ajouter après chaque élément</param>
+        /// <returns>l'énumérable sous forme textuelle</returns>
+        /// <remarks>
+        ///     Si un argument facultatif est null, on le remplace par sa valeur 
+        ///     par défaut.
+        /// </remarks>
+        public static string EnTexte<T>(this IEnumerable<T> elems, string séparateur = " ",
+            string texteAvant = "", string texteAprès = "")
+        {
+        }
+    }
+}
