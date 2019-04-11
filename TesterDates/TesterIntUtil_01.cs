523db26d7c724f137da88e5d55660710bbf6f07b
Veronique Giguere <0252497@cstjean.qc.ca>
2019-04-11 12:35:29 -0400

    test 71

---
+++ TesterDates/TesterIntUtil_01.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+using static Prog2.IntUtil;
+
+namespace Prog2
+{
+    [TestClass]
+    public partial class TesterIntUtil
+    {
+        [TestMethod]
+        public void _01_ComparerAvec()
+        {
+            AreEqual(1, 10.ComparerAvec(9));
+            AreEqual(0, 10.ComparerAvec(10));
+            AreEqual(-1, 10.ComparerAvec(11));
+        }
+    }
+}
